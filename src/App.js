7508b6d3655d9b9aafcd0d97af89149adeec3c17
cailu6 <cll0815@163.com>
2018-01-28 10:46:59 +0800

    react-cai

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import TabControl from './component/tab/Tab'
 // class App extends Component {
 //   render() {
 //     return (
-//       <div className="Ap9999p">
+//       <div className="App">
 //         <header className="App-header">
 //           <img src={logo} className="App-logo" alt="logo" />
 //           <h1 className="App-title">Welcome to React</h1>
